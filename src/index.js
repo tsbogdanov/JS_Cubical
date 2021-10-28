08a1ff366249e527990d4fe7863e6b0db3ad63b6
tsbogdanov <tsbogdanov@gmail.com>
2021-10-28 08:21:12 -0400

    install express and run server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
-
-
-
 const port = 5000;
 const app = express();
-app.listen(port, () => console.log(`Life is good on port ${port}...`));
+
+
+app.all('/', (req, res) => {
+    res.write("It's working");
+    res.end();
+})
+
+
+app.listen(port, () => console.log(`Life is good!!! Yor server is running on port ${port}...`));
