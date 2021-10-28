961d9906a8d46a5a961667a090c67517df158b10
tsbogdanov <tsbogdanov@gmail.com>
2021-10-28 09:45:22 -0400

    Add css styles and public folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const port = 5000;
 const app = express();
+const path = require('path');
 const initHandlebars = require('./config/handlebars.js');
 
 initHandlebars(app);
+
+app.use(express.static(path.resolve(__dirname, './public')))
+
 app.all('/', (req, res) => {
     res.render('index');
 
